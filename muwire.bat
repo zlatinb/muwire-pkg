a088905f33d523b19e157dec840b0ec855f96b96
Zlatin Balevsky <zlatinb@gmail.com>
2019-10-09 10:42:37 -0700

    start MW in IDLE priority on Windows

--- muwire.bat
+++ muwire.bat
@@ -1,4 +1,4 @@
 @echo off
 cd %~dp0
-start jre\bin\javaw.exe -Xmx512M -cp "MuWire.jar;unnamed.jar;jbigi.jar" -Djava.library.path=".;lib" -DembeddedRouter=true -DupdateType=exe com.muwire.gui.Launcher 
+start /LOW jre\bin\javaw.exe -Xmx512M -cp "MuWire.jar;unnamed.jar;jbigi.jar" -Djava.library.path=".;lib" -DembeddedRouter=true -DupdateType=exe com.muwire.gui.Launcher 
 exit
