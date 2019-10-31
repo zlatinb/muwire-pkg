f7d953676d1f6866d0961b9d2d177d24cc9bb76f
Zlatin Balevsky <zlatinb@gmail.com>
2019-10-31 13:35:34 +0000

    set dock name

--- muwire-mac.sh
+++ muwire-mac.sh
@@ -3,5 +3,5 @@
 cd "$(dirname "$0")"
 mkdir -p "$HOME/Library/Application Support/MuWire"
 cp -R certificates "$HOME/Library/Application Support/MuWire"
-jre/bin/java -Xdock:icon=../Resources/MuWire-128x128.png -cp "jbigi.jar:MuWire.jar:unnamed.jar" -DembeddedRouter=true -DupdateType=mac -Djava.util.logging.config.file=1_logging.properties com.muwire.gui.Launcher
+jre/bin/java -Xdock:name=MuWire -Xdock:icon=../Resources/MuWire-128x128.png -cp "jbigi.jar:MuWire.jar:unnamed.jar" -DembeddedRouter=true -DupdateType=mac -Djava.util.logging.config.file=1_logging.properties com.muwire.gui.Launcher
 
