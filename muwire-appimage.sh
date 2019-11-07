f0ea41adaedf10ff6036fb956278fdbae6d1485a
Zlatin Balevsky <zlatinb@gmail.com>
2019-11-06 22:30:13 -0800

    test if directory exists

--- muwire-appimage.sh
+++ muwire-appimage.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-mkdir ${HOME}/.MuWire
-cp -R ${HERE}/certificates ${HOME}/.MuWire
+if [ ! -d ${HOME}/.MuWire]; then
+    mkdir ${HOME}/.MuWire
+    cp -R ${HERE}/certificates ${HOME}/.MuWire
+fi
 cd ${HOME}/.MuWire
 ${HERE}/jre/bin/java -Xmx512m -cp "${HERE}/jbigi.jar:${HERE}/MuWire.jar:${HERE}/unnamed.jar" -DembeddedRouter=true -DupdateType=appimage -Djava.util.logging.config.file=${HERE}/1_logging.properties com.muwire.gui.Launcher
 
