ff51d82e9b29cc8b24612d7c5b74fbcfb1f5511d
Zlatin Balevsky <zlatinb@gmail.com>
2020-04-23 20:26:17 +0100

    wip on JPackage-based windows installer

--- jpackage.sh
+++ jpackage.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+
+RES_DIR=../i2p.i2p/installer/resources
 LIB_DIR=lib-tmp
 JVM_DIR=dist/win
 I2P_JARS=../i2p.i2p/pkg-temp
@@ -8,7 +10,7 @@ DEST_DIR=MuWire
 MW_DIR=../muwire
 VERSION=$(cat VERSION)
 
-rm -rf $LIB_DIR $DEST_DIR
+rm -rf $LIB_DIR $DEST_DIR build
 
 echo "preparing lib dir for jpackage"
 
@@ -27,6 +29,7 @@ cp "${I2P_JARS}"/lib/jbigi.jar $LIB_DIR
 echo "executing jpackage"
 
 "${JAVA_HOME}"/bin/jpackage --name MuWire \
+       --icon MuWire.ico \
         --app-version $VERSION \
         --description "Easy Anonymous File-Sharing" \
         --java-options "-Djava.library.path=\"\$ROOTDIR;\$ROOTDIR\\app\"" \
@@ -42,3 +45,18 @@ echo "executing jpackage"
 
 rm -rf $LIB_DIR
 
+echo "copying files"
+mkdir -p build/pkg
+
+cp MuWire.ico build
+cp GPLv3.txt build/GPLv3-ud.txt
+unix2dos build/GPLv3-ud.txt
+cp version.nsi build
+
+cp -R $RES_DIR/certificates build/pkg
+cp $RES_DIR/countries.txt build/pkg
+cp $RES_DIR/continents.txt build/pkg
+cp $RES_DIR/GeoLite2-Country.mmdb.gz build/pkg
+gunzip build/pkg/GeoLite2-Country.mmdb.gz
+
+cp -R $DEST_DIR/* build/pkg
